16c1115711bac885ee407da1329d449703a9821b
João Tanaca <jtanacar@gmail.com>
2021-01-13 21:26:38 -0300

    Removendo componentes de dowload

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,11 @@
 import { Router } from 'express';
-import multer from 'multer';
 
-import uploadConfig from './config/upload';
 import FloodingController from './controllers/FloodingController';
 
 const routes = Router();
-const upload = multer(uploadConfig);
 
 routes.get('/floodings', FloodingController.index);
 routes.get('/floodings/:id', FloodingController.show);
-routes.post('/floodings', upload.array('images'), FloodingController.create);
+routes.post('/floodings', FloodingController.create);
 
 export default routes;
--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
 app.use(errorHandler);
 
 app.listen(3333);
